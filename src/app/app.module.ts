5159669ed3aa09a78cf508aa90e8a83d7e516274
Wolfreak <jrmyargoud@gmail.com>
2021-04-05 11:12:37 +0200

    Create Todo app
    It's a little ToDo app for myself

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostService } from './services/post.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'posts', component: PostListComponent},
+  {path: 'new', component: NewPostComponent},
+  {path: '',   redirectTo: '/posts', pathMatch: 'full'},
+]
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewPostComponent,
+    PostListComponent,
+    PostListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    PostService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
